fe5e1827692599297afdc346210bb646af6973d3
JKapturkiewicz <kuba.kapek@gmail.com>
2023-10-16 09:26:21 +0200

    Change file extension to .tsx and declare function type

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Container } from './components/Container/Container';
+
+function App(): React.ReactElement {
+   return (
+      <Container>
+         <h1>JoBoard 🛹</h1>
+      </Container>
+   );
+}
+
+export default App;

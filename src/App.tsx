0ac000f10449aaab74a568980b50725e301c4c63
JKapturkiewicz <kuba.kapek@gmail.com>
2023-10-20 18:43:48 +0200

    Add SearchSection

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Container } from './components/Container/Container';
 import JobList from './components/JobList/JobList';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SearchSection from './components/search/SearchSection';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App(): React.ReactElement {
       <QueryClientProvider client={queryClient}>
          <Container>
             <h1>JoBoard 🛹</h1>
+            <SearchSection />
             <JobList />
          </Container>
       </QueryClientProvider>

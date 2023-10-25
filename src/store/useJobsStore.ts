0029a1d583956d3950240aeed21274feca8c95f7
JKapturkiewicz <kuba.kapek@gmail.com>
2023-10-25 16:09:57 +0200

    Update store

--- src/store/useJobsStore.ts
+++ src/store/useJobsStore.ts
@@ -1,16 +1,25 @@
 import { create } from 'zustand';
 import type { IJobs } from '../services/types';
 
+type JobType = {
+   fullTime: boolean;
+   contract: boolean;
+   partTime: boolean;
+   freelance: boolean;
+};
+
 interface JobsStore {
    jobs: IJobs;
    filteredJobs: IJobs;
    searchTitle: string;
    searchLocation: string;
+   jobType: JobType;
 
    setJobs: (value: IJobs) => void;
    clearSearch: () => void;
    setSearchTitle: (value: string) => void;
    setSearchLocation: (value: string) => void;
+   setJobType: (key: keyof JobType, value: boolean) => void;
 }
 
 const useJobsStore = create<JobsStore>((set) => ({
@@ -18,6 +27,7 @@ const useJobsStore = create<JobsStore>((set) => ({
    filteredJobs: [],
    searchTitle: '',
    searchLocation: '',
+   jobType: { fullTime: false, contract: false, partTime: false, freelance: false },
 
    setJobs: (value) => set({ jobs: value, filteredJobs: value }),
    clearSearch: () =>
@@ -30,6 +40,12 @@ const useJobsStore = create<JobsStore>((set) => ({
       set({ searchLocation: value });
       setFilteredJobs(set);
    },
+   setJobType: (key, value) => {
+      set((state) => ({
+         jobType: { ...state.jobType, [key]: value },
+      }));
+      setFilteredJobs(set);
+   },
 }));
 
 const setFilteredJobs = (set: (state: (prevState: JobsStore) => Partial<JobsStore>) => void) => {
@@ -39,7 +55,11 @@ const setFilteredJobs = (set: (state: (prevState: JobsStore) => Partial<JobsStor
             (!state.searchTitle ||
                job.title.toLowerCase().includes(state.searchTitle.toLowerCase())) &&
             (!state.searchLocation ||
-               job.city.toLowerCase().includes(state.searchLocation.toLowerCase()))
+               job.city.toLowerCase().includes(state.searchLocation.toLowerCase())) &&
+            (!state.jobType.fullTime || job.jobType === 'Full-time') &&
+            (!state.jobType.contract || job.jobType === 'Contract') &&
+            (!state.jobType.partTime || job.jobType === 'Part-time') &&
+            (!state.jobType.freelance || job.jobType === 'Freelance')
          );
       }),
    }));

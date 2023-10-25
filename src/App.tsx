4bf14fa4459d8b6a251d61d371bbf4266bcf1f74
JKapturkiewicz <kuba.kapek@gmail.com>
2023-10-25 16:17:28 +0200

    Implement Containers and JobFiltersSection

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { Container } from './components/Container/Container';
 import JobList from './components/JobList/JobList';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SearchSection from './components/search/SearchSection';
+import JobFiltersSection from './components/JobFilters/JobFiltersSection';
+import { ContainerFilters } from './components/ContainerFilters/ContainerFilters';
+import { ContainerJobs } from './components/ContainerJobs/ContainerJobs';
 
 const queryClient = new QueryClient();
 
@@ -11,9 +14,15 @@ function App(): React.ReactElement {
    return (
       <QueryClientProvider client={queryClient}>
          <Container>
-            <h1>JoBoard 🛹</h1>
-            <SearchSection />
-            <JobList />
+            <ContainerFilters>
+               <h1>👾 JO-BOARD</h1>
+               <JobFiltersSection />
+            </ContainerFilters>
+
+            <ContainerJobs>
+               <SearchSection />
+               <JobList />
+            </ContainerJobs>
          </Container>
       </QueryClientProvider>
    );

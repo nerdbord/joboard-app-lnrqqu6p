25df3eb3ed7a0e816edf690552d151e5cd7260c6
JKapturkiewicz <kuba.kapek@gmail.com>
2023-10-26 16:50:22 +0200

    Implement salary filtering

--- src/components/JobFilters/JobFilter.tsx
+++ src/components/JobFilters/JobFilter.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import styles from './JobFilter.module.scss';
 import CheckBox, { Option } from '../CheckBox/CheckBox';
+import Slider from '../Slider/Slider';
 
 interface Props {
    label: string;
-   options: Array<Option>;
+   options: Array<Option> | {value: number, setValue: (value: number) => void};
 }
 
 const JobFilter: React.FC<Props> = ({ label, options }) => {
    return (
       <div className={styles.container}>
          <span className={styles.title}>{label}</span>
-         <div className={styles.optionsContainer}>
-            {options.map((option, idx) => (
-               <CheckBox key={idx} option={option} />
-            ))}
-         </div>
+         {Array.isArray(options) ? (
+            <div className={styles.optionsContainer}>
+               {options.map((option, idx) => (
+                  <CheckBox key={idx} option={option} />
+               ))}
+            </div>
+         ) : (
+            <Slider value={options.value} setValue={options.setValue}/>
+         )}
       </div>
    );
 };

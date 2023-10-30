c3bd3ae509e0b1e5b4a71b6f007d1ce90a1ec890
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-10-30 13:00:57 +0100

    Refactoring after CR

--- src/components/JobOffer/JobOffer.tsx
+++ src/components/JobOffer/JobOffer.tsx
@@ -3,6 +3,7 @@ import { useGetOfferById } from '../../services/queries';
 import useJobsStore from '../../store/useJobsStore';
 import style from './JobOffer.module.scss';
 import closeIcon from '../../assets/close.svg';
+import { dayInterval } from '../../services/utils';
 
 const JobOffer: React.FC = () => {
    const { offer, currentOfferId, setIsOfferWindowOper, isOfferWindowOpen } = useJobsStore();
@@ -19,19 +20,6 @@ const JobOffer: React.FC = () => {
 
    const handleOnCloseClick = () => {
       setIsOfferWindowOper(!isOfferWindowOpen);
-   };
-
-   const dayInterval = (created: string) => {
-      const creationDate = new Date(created);
-      const nowDate = new Date();
-      const differenceInTime = nowDate.getTime() - creationDate.getTime();
-      const dayInMiliseconds = 1000 * 60 * 60 * 24;
-      const differenceInDays = Math.ceil(differenceInTime / dayInMiliseconds);
-      return differenceInDays > 1
-         ? differenceInDays + ' days ago'
-         : differenceInDays === 1
-         ? ' day ago'
-         : 'today';
    };
 
    return (

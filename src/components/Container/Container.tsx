2adf344d65c381593644066dcc7530cbac11af24
agnbart <polpumi@gmail.com>
2023-11-03 16:04:11 +0100

    test: setup of test, CheckBox testing

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,9 +3,5 @@ import styles from './Container.module.scss';
 import { PropsWithChildren } from '../../services/types';
 
 export const Container: React.FC<PropsWithChildren> = ({ children }) => {
-   return (
-      <div className={styles['container']}>
-         {children}
-      </div>
-   );
+   return <div className={styles['container']}>{children}</div>;
 };

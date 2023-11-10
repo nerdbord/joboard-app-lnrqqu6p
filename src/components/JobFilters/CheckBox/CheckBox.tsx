8411330f1c284cf5324aa95221e07b1019d3afee
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-10 19:04:08 +0100

    Add offerKeyName to the data-test-option attribute

--- src/components/JobFilters/CheckBox/CheckBox.tsx
+++ src/components/JobFilters/CheckBox/CheckBox.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './CheckBox.module.scss';
 import CheckBoxTrue from '../../icons/CheckBox/CheckBoxTrue';
 import CheckBoxFalse from '../../icons/CheckBox/CheckBoxFalse';
+import { IJob } from '../../../services/types';
 
 export type Option = {
    keyName: string;
@@ -12,15 +13,21 @@ export type Option = {
 
 interface Props {
    option: Option;
+   offerKeyName: keyof IJob;
 }
 
-const CheckBox: React.FC<Props> = ({ option }) => {
+const CheckBox: React.FC<Props> = ({ option, offerKeyName }) => {
    const { keyName, label, value, setValue } = option;
    const updateValue = () => {
       setValue(keyName, !value);
    };
    return (
-      <div className={styles.container} onClick={updateValue}>
+      <div
+         className={styles.container}
+         onClick={updateValue}
+         data-testid="filter-option"
+         data-test-option={JSON.stringify({ offerKeyName: offerKeyName, offerOption: label, value: value })}
+      >
          <div className={styles.icon}>{value ? <CheckBoxTrue /> : <CheckBoxFalse />}</div>
          <span>{label}</span>
       </div>

ac2b65ce9285ffbe1271361fb4ad0693e1ad7e99
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-10 19:03:27 +0100

    Pass offerKeyName to child component

--- src/components/JobFilters/JobFilter.tsx
+++ src/components/JobFilters/JobFilter.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import styles from './JobFilter.module.scss';
-import CheckBox, { Option } from '../CheckBox/CheckBox';
-import Slider from '../Slider/Slider';
+import CheckBox, { Option } from './CheckBox/CheckBox';
+import Slider from './Slider/Slider';
+import type { IJob } from '../../services/types';
 
 interface Props {
    label: string;
+   offerKeyName: keyof IJob;
    options: Array<Option> | { value: number; setValue: (value: number) => void };
 }
 
-const JobFilter: React.FC<Props> = ({ label, options }) => {
+const JobFilter: React.FC<Props> = ({ label, offerKeyName, options }) => {
    return (
-      <div className={styles.container}>
+      <div
+         className={styles.container}
+         data-testid={`filter-${label.toLowerCase().replace(' ', '-')}`}
+      >
          <span className={styles.title}>{label}</span>
          {Array.isArray(options) ? (
             <div className={styles.optionsContainer}>
                {options.map((option, idx) => (
-                  <CheckBox key={idx} option={option} />
+                  <CheckBox key={idx} option={option} offerKeyName={offerKeyName} />
                ))}
             </div>
          ) : (

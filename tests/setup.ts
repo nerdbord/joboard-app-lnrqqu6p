7c002acb7ddfae0154890984b068fb379d03c06b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-11 17:37:37 +0100

    Setup msw server

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,41 +2,34 @@ import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node';
-import { mswHandlers } from './utils/queries';
-
-Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: vi.fn().mockImplementation((query) => ({
-       matches: false,
-       media: query,
-       onchange: null,
-       addListener: vi.fn(), // deprecated
-       removeListener: vi.fn(), // deprecated
-       addEventListener: vi.fn(),
-       removeEventListener: vi.fn(),
-       dispatchEvent: vi.fn(),
-    })),
- });
-
-// export const server = setupServer(...mswHandlers);
+import { server } from './utils/server';
 
 beforeAll(() => {
-//    server.listen();
-   // console.log('Start server msw listening')
+   server.listen();
 });
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => {
    cleanup();
-//    server.resetHandlers();
+   server.resetHandlers();
 });
 
 // Clean up after the tests are finished.
 afterAll(() => {
-//    server.close();
-   // console.log('Close msw server')
+   server.close();
 });
 
-
+Object.defineProperty(window, 'matchMedia', {
+   writable: true,
+   value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+   })),
+});

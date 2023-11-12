016c239891701bef1300c56bf9c47c592c7de5a8
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-11-12 15:49:09 +0100

    Fixing gh pages deploy

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -1,25 +1,25 @@
-import {afterEach} from 'vitest';
-import {cleanup} from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+// import '@testing-library/jest-dom/vitest';
 import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node'
-import { mswHandlers } from './testUtils'
+import { setupServer } from 'msw/node';
+import { mswHandlers } from './testUtils';
 
-console.log('Start setup.js')
-export const server = setupServer(...mswHandlers)
+console.log('Start setup.js');
+export const server = setupServer(...mswHandlers);
 
 beforeAll(() => {
-    server.listen()
-    console.log('Start server msw listening')
-})
+   server.listen();
+   console.log('Start server msw listening');
+});
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => {
-    cleanup();
-    server.resetHandlers()}
-    )
+   cleanup();
+   server.resetHandlers();
+});
 // Clean up after the tests are finished.
 afterAll(() => {
-    server.close()
-    console.log('Close msw server')
-})
+   server.close();
+   console.log('Close msw server');
+});

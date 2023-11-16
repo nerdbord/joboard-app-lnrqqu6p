0a160488927f30c773b79a68dbcf89985964a2b6
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-16 17:59:48 +0100

    Use modal component

--- src/components/JobList/JobList.tsx
+++ src/components/JobList/JobList.tsx
@@ -4,11 +4,11 @@ import useJobsStore from '../../store/useJobsStore';
 import styles from './JobList.module.scss';
 import JobCard from '../JobCard/JobCard';
 import JobOffer from '../JobOffer/JobOffer';
-import JobOfferContainer from '../JobOffer/JobOfferContainer';
+import Modal from '../Modal/Modal';
 
 const JobList: React.FC = () => {
    const { isError, isPending, data } = useGetJobs();
-   const { setJobs, filteredJobs, isOfferWindowOpen } = useJobsStore();
+   const { setJobs, filteredJobs } = useJobsStore();
 
    useEffect(() => {
       data && setJobs(data);
@@ -26,11 +26,9 @@ const JobList: React.FC = () => {
       <div className={styles.jobOffersList} data-testid="jobs-container">
          {filteredJobs && filteredJobs.map((offer) => <JobCard key={offer._id} offer={offer} />)}
 
-         {isOfferWindowOpen && (
-            <JobOfferContainer>
-               <JobOffer />
-            </JobOfferContainer>
-         )}
+         <Modal>
+            <JobOffer />
+         </Modal>
       </div>
    );
 };

a7c6781a24b265a643b434156205029dfcfc40ce
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-16 18:00:21 +0100

    Update offer appearance

--- src/components/JobOffer/JobOffer.tsx
+++ src/components/JobOffer/JobOffer.tsx
@@ -8,7 +8,23 @@ import { dayInterval } from '../../services/utils';
 const JobOffer: React.FC = () => {
    const { offer, currentOfferId, setIsOfferWindowOper, isOfferWindowOpen } = useJobsStore();
    const { isError, isPending } = useGetOfferById(currentOfferId);
-   console.log(offer);
+   const {
+      city,
+      companyName,
+      country,
+      createdAt,
+      currency,
+      description,
+      image,
+      jobType,
+      offerUrl,
+      salaryFrom,
+      salaryTo,
+      seniority,
+      technologies,
+      title,
+      workLocation,
+   } = offer;
 
    if (isError) {
       return <div>Error occurred while fetching jobs.</div>;
@@ -23,101 +39,74 @@ const JobOffer: React.FC = () => {
    };
 
    return (
-      <>
-         {Array(offer).map(
-            ({
-               city,
-               companyName,
-               country,
-               createdAt,
-               currency,
-               description,
-               image,
-               jobType,
-               offerUrl,
-               salaryFrom,
-               salaryTo,
-               seniority,
-               technologies,
-               title,
-               workLocation,
-               _id,
-            }) => {
-               return (
-                  <React.Fragment key={_id}>
-                     <header className={style.headerContainer} key={_id}>
-                        <img src={image} alt="Offer logo" className={style.logo} />
-                        <div className={style.titleBox}>
-                           <h2 className={style.title}>{title.toUpperCase()}</h2>
-                           <p className={style.technologies}>
-                              {technologies
-                                 .map((technology) => technology.toUpperCase())
-                                 .join(' ・ ')}
-                           </p>
-                        </div>
-                     </header>
-                     <section className={style.textSection}>
-                        <div className={style.textContainer}>
-                           <h3 className={style.textTitle}>{title}</h3>
-                           <p className={style.description}>{description}</p>
-                        </div>
-                        <div className={style.detailsContainer}>
-                           <div className={style.buttonBox}>
-                              <button className={style.btn}>
-                                 <a href={offerUrl}>Visit offer ➔</a>
-                              </button>
-                           </div>
-                           <div className={style.infoColumnContainer}>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Added</p>
-                                 <p className={style.infoDescription}>{dayInterval(createdAt)}</p>
-                              </div>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Company</p>
-                                 <p className={style.infoDescription}>{companyName}</p>
-                              </div>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Seniority</p>
-                                 <p className={style.infoDescription}>{seniority}</p>
-                              </div>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Location</p>
-                                 <p className={style.infoDescription}>
-                                    <span>{city},</span>
-                                    <span>{country}</span>
-                                 </p>
-                              </div>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Job type</p>
-                                 <p className={style.infoDescription}>
-                                    <span>{workLocation},</span>
-                                    <span>{jobType}</span>
-                                 </p>
-                              </div>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Contract</p>
-                                 <p className={style.infoDescription}>{jobType}</p>
-                              </div>
-                              <div className={style.infoBox}>
-                                 <p className={style.infoTitle}>Salary</p>
-                                 <p className={style.infoDescription}>
-                                    {salaryFrom} – {salaryTo} {currency} netto
-                                 </p>
-                              </div>
-                           </div>
-                        </div>
-                     </section>
-                     <img
-                        src={closeIcon}
-                        alt="Close icon"
-                        className={style.closeIcon}
-                        onClick={handleOnCloseClick}
-                     />
-                  </React.Fragment>
-               );
-            },
-         )}
-      </>
+      <div className={style.container}>
+         <header className={style.headerContainer}>
+            <img src={image} alt="Offer logo" className={style.logo} />
+            <div className={style.titleBox}>
+               <h2 className={style.title}>{title.toUpperCase()}</h2>
+               <p className={style.technologies}>
+                  {technologies.map((technology) => technology.toUpperCase()).join(' ・ ')}
+               </p>
+            </div>
+         </header>
+         <section className={style.textSection}>
+            <div className={style.textContainer}>
+               <h3 className={style.textTitle}>{title}</h3>
+               <p className={style.description}>{description}</p>
+            </div>
+            <div className={style.detailsContainer}>
+               <div className={style.buttonBox}>
+                  <button className={style.btn}>
+                     <a href={offerUrl}>Visit offer ➔</a>
+                  </button>
+               </div>
+               <div className={style.infoColumnContainer}>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Added</p>
+                     <p className={style.infoDescription}>{dayInterval(createdAt)}</p>
+                  </div>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Company</p>
+                     <p className={style.infoDescription}>{companyName}</p>
+                  </div>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Seniority</p>
+                     <p className={style.infoDescription}>{seniority}</p>
+                  </div>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Location</p>
+                     <p className={style.infoDescription}>
+                        <span>{city},</span>
+                        <span>{country}</span>
+                     </p>
+                  </div>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Job type</p>
+                     <p className={style.infoDescription}>
+                        <span>{workLocation},</span>
+                        <span>{jobType}</span>
+                     </p>
+                  </div>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Contract</p>
+                     <p className={style.infoDescription}>{jobType}</p>
+                  </div>
+                  <div className={style.infoBox}>
+                     <p className={style.infoTitle}>Salary</p>
+                     <p className={style.infoDescription}>
+                        {salaryFrom} – {salaryTo} {currency} netto
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <img
+            src={closeIcon}
+            alt="Close icon"
+            className={style.closeIcon}
+            onClick={handleOnCloseClick}
+         />
+      </div>
    );
 };
 
